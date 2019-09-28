bcc9f349c055576b8e0c2c2329729abb43846101
UrielYair <uriel91@gmail.com>
2019-09-28 22:09:58 +0300

    Add Prototype exercise.

---
+++ src/com/uriel/DesignPatterns/prototype_exercise/Line.java
@@ -0,0 +1,17 @@
+package com.uriel.DesignPatterns.prototype_exercise;
+
+class Line
+{
+    public Point start, end;
+
+    public Line(Point start, Point end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+
+    public Line deepCopy()
+    {
+        // todo
+    }
+}
---
+++ src/com/uriel/DesignPatterns/prototype_exercise/Point.java
@@ -0,0 +1,12 @@
+package com.uriel.DesignPatterns.prototype_exercise;
+
+class Point{
+    public int x, y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
